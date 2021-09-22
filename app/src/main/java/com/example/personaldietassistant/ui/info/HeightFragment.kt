bd856beffb46ca89326a7f57bf7851bf2cc90a0b
doganzekiye <zekiyedogan17@gmail.com>
2021-09-22 18:34:04 +0300

    Design height layout. Implement number picker for height.

--- app/src/main/java/com/example/personaldietassistant/ui/info/HeightFragment.kt
+++ app/src/main/java/com/example/personaldietassistant/ui/info/HeightFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.NumberPicker
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import com.example.personaldietassistant.R
@@ -13,19 +15,51 @@ import com.example.personaldietassistant.ui.base.BaseFragment
 class HeightFragment : BaseFragment() {
     lateinit var binding: FragmentHeightBinding
 
+    lateinit var pickedHeight: TextView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_height, container, false)
         return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        onClickNumberPicker()
         binding.btnHeightAccept.setOnClickListener {
             findNavController().navigate(R.id.action_heightFragment_to_weightFragment)
         }
         setToolbar(binding.toolbar.root, title = "Boyunu Gir", onClick = {
             findNavController().navigateUp()
         })
+    }
+
+    fun onClickNumberPicker() {
+         binding.npHeight.apply {
+            maxValue = 220
+            minValue = 120
+            value = 150
+            wrapSelectorWheel = false
+        }
+
+        binding.npHeight.setOnValueChangedListener { picker, oldVal, newVal ->
+            binding.tvPickedHeight.text = (String.format("My height is %s.%s cm",  newVal,binding.npHeightDecimal.value))
+        }
+
+        binding.npHeightDecimal.apply {
+            maxValue = 9
+            minValue = 0
+            value = 0
+            wrapSelectorWheel = false
+        }
+
+        binding.tvPickedHeight.text = (String.format("My height is %s.%s cm",  binding.npHeight.value,binding.npHeightDecimal.value))
+
+        binding.npHeightDecimal.setOnValueChangedListener { picker, oldVal, newVal ->
+            binding.tvPickedHeight.text = (String.format("My height is %s.%s cm", binding.npHeight.value ,newVal))
+
+        }
+
     }
 }
