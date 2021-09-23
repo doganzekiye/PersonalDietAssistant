b407f27475ba21d8207afbb2a5b0e1c4f86f4679
doganzekiye <zekiyedogan17@gmail.com>
2021-09-23 16:55:17 +0300

    General commit.

--- app/src/main/java/com/example/personaldietassistant/ui/info/AgeFragment.kt
+++ app/src/main/java/com/example/personaldietassistant/ui/info/AgeFragment.kt
@@ -24,6 +24,7 @@ class AgeFragment : BaseFragment(), OnSnapPositionChangeListener {
 
     override fun onCreateView(
         inflater: LayoutInflater,
+
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
