5d7680c695713a40861d16b9ffcc0094538219dd
doganzekiye <zekiyedogan17@gmail.com>
2021-10-08 13:32:22 +0300

    A new function setTargetWeight is calling a function from InfoScreeenViewModel for calculating target weight.

--- app/src/main/java/com/example/personaldietassistant/ui/info/HeightFragment.kt
+++ app/src/main/java/com/example/personaldietassistant/ui/info/HeightFragment.kt
@@ -28,6 +28,7 @@ class HeightFragment : BaseFragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setTargetWeight(getString(R.string.female))
         setOnClick()
         setStepToolbar(binding.toolbar.root, stepSelectedCount = 5, stepTotalCount = 8, onClick = {
             findNavController().navigateUp()
@@ -65,6 +66,13 @@ class HeightFragment : BaseFragment() {
         }
         binding.btnHeightAccept.setOnClickListener {
             findNavController().navigate(R.id.action_heightFragment_to_weightFragment)
+        }
+    }
+    private fun setTargetWeight(gender: String) {
+        if (gender == getString(R.string.female)) {
+            viewModel.setTargetFemale()
+        } else if (gender == getString(R.string.male)) {
+            viewModel.setTargetMale()
         }
     }
 }
