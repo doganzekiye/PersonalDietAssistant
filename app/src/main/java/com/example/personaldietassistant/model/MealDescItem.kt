90e6db27f67cb37c6826051e7c22451f8ef9b9f3
doganzekiye <zekiyedogan17@gmail.com>
2021-10-20 18:53:52 +0300

    Created a model will ve added to database.

---
+++ app/src/main/java/com/example/personaldietassistant/model/MealDescItem.kt
@@ -0,0 +1,9 @@
+package com.example.personaldietassistant.model
+
+data class MealDescItem(
+    val name: String? = "TEST",
+    val desc: String? = "TEST",
+    val carbsCal: Double = 0.0,
+    val fatCal: Double = 0.0,
+    val proteinCal: Double = 0.0
+)
